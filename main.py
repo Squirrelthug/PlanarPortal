dbf87233507e2231aa68e3075e8513a9f152420d
Ratatoskyr <squirrelthug@gmail.com>
2023-07-16 23:27:01 -0700

    just getting started and didn't want to leave the page blank. Will be working on this slowly. I'm not a commie, I just like free stuff and sharing.

---
+++ main.py
@@ -0,0 +1,6 @@
+from WotC import Magic
+from commonsense import Nothing
+from myopinion import DGAF
+
+
+print("If you're a wizard and you're reading this, GFY.")
---
+++ menu.py
@@ -0,0 +1,8 @@
+from turtle import Turtle, Screen
+
+sam = Turtle()
+screen = Screen()
+
+initialization_sequence()
+
+screen.exitonclick()
